86c24fff470925c0a94ad4a758995231f39f9b1f
Mykyta Matushyn <nikita@selfdecode.com>
2022-07-30 17:18:32 +0000

    2022.07.26: slight refactoring & doc:  - added comments of code blocks with Abdallah & Adriano  - slight tidying up  - optimized `get_sample_index`, which slightly optimizes the full thing

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# imputation-selphi
+# selphi-imputation_kuk-optimizing
+Refactoring &amp; optimizing Abdallah's &amp; Adriano's `rd-imputation-selphi` code
+
+
+<br>
+
+## Source code and data from Adriano and Abdallah
+
+Relevant | Date | Source | Comment
+--- | --- | --- | ---
+:heavy_check_mark: | July 19th 2022 | https://s3.console.aws.amazon.com/s3/buckets/imputation-project?region=us-east-1&prefix=shared/selphi_fdraft/&showversions=false | [Aby] _All data included, but you just need to change the path for the genetic map file._
+:white_circle: | July 28th 2022 | https://s3.console.aws.amazon.com/s3/buckets/imputation-project?region=us-east-1&prefix=shared/imputation_prj_versions/1/ | [Aby] _Done re structuring. Also you will find chr1 and chr20 already prepared for selphi imputation. For results checking, you have to add beagle imputed 292 data for chr20 because we didn't do them yet. You will find a notebook that convert it to zip array that is used in check_results.ipynb_ 
--- SelphiFirstDraft-impute_all_chr.py
+++ SelphiFirstDraft-impute_all_chr.py
@@ -4,13 +4,14 @@
 # In[ ]:
 
 
+from typing import List
 import pandas as pd
 import numpy as np
 import zarr
 import pickle
 from tqdm import trange
 
-from utils import BidiBurrowsWheelerLibrary
+from utils import BidiBurrowsWheelerLibrary, forced_open
 from data_utils import get_sample_index, remove_all_samples
 
 from variations import run_hmm
@@ -26,7 +27,9 @@ SI_data_folder = f"./new_data/SI_data/{chrom}"
 
 # In[5]:
 
-
+"""
+Returns a list of only first occurences of unique values from the input sequence (omits duplicates)
+"""
 def f7(seq):
     seen = set()
     seen_add = seen.add
@@ -53,19 +56,29 @@ len(samples)
 # Load Data
 
 with open(f"{SI_data_folder}/chip_id_list.txt", "rb") as fp:   # Unpickling
-    chip_id_list = pickle.load(fp)
+    chip_id_list: List[str] = pickle.load(fp)
 with open(f"{SI_data_folder}/full_id_list_full.txt", "rb") as fp:   # Unpickling
-    full_id_list = pickle.load(fp)
+    full_id_list: List[str] = pickle.load(fp)
 with open(f"{SI_data_folder}/original_indicies_full.txt", "rb") as fp:   # Unpickling
-    original_indicies = pickle.load(fp)
+    original_indicies: List[int] = pickle.load(fp)
 chr_length = len(full_id_list)
 
+"""
+`chip_id_list` contains ids available in the input sample
+`full_id_list` contains ids available in samples from the reference panel
+
+all ids in `chip_id_list` are included in `full_id_list`, while `original_indicies`
+is the list of indicies of ids in the `chip_id_list` as they appear in `full_id_list`
+"""
+assert len(chip_id_list) == len(original_indicies)
+for i in range(len(original_indicies)):
+    oi = original_indicies[i]
+    assert chip_id_list[i] == full_id_list[oi]
 
 # In[10]:
 
 
 # LOAD GENETIC MAP
-import os
 # PUT genetic map path here that beagle uses
 genetic_map_path = f"/home/nikita/s3-sd-platform-dev/efs/service/genome-files-processing/resource_files/genetic_map_plink/plink.{chrom}.GRCh38.map"
 
@@ -80,7 +93,6 @@ map_pops = ["default",]
 recomb_maps = {x:z for (x,z) in [[x, {k:v for (k,v) in [[row[3],row[2]] for _, row in pd.read_csv(genetic_map_path, sep=" ", comment="#").iterrows()]}] for x in map_pops]}
 chip_positions_dedup = [int(x.split('-')[1]) for x in chip_id_list]
 num_obs = len(chip_positions_dedup)
-distances_cm = [get_cm(recomb_maps, x) for x in chip_positions_dedup]
 
 
 # In[11]:
@@ -95,7 +107,7 @@ genetic_map['chr'] = genetic_map.chr.fillna(20.0).astype(int)
 genetic_map = genetic_map.reset_index().rename({'index':'pos'},axis=1)
 genetic_map = genetic_map[genetic_map['pos'].isin(chip_positions_dedup)].reset_index(drop=True)
 distances_cm = genetic_map.cM
-distances_cm = list(distances_cm)
+distances_cm: List[float] = list(distances_cm)
 
 print("processed genetic map:")
 print(genetic_map)
@@ -124,7 +136,7 @@ def get_std(avg_length, a=25):
 # In[13]:
 
 
-ref_panel_full_array_full_packed = zarr.load("./new_data/SI_data/chr1/reference_panel.30x.hg38_chr1_noinfo_full_nppacked.zip")
+ref_panel_full_array_full_packed: np.ndarray = zarr.load("./new_data/SI_data/chr1/reference_panel.30x.hg38_chr1_noinfo_full_nppacked.zip") # type: ignore # enforce the type
 
 print("loaded the full reference panel")
 print(f"shape: {ref_panel_full_array_full_packed.shape}")
@@ -134,35 +146,59 @@ print(ref_panel_full_array_full_packed.__repr__())
 
 import time
 start_time = time.time()
+hmm_running_times = []
 
-from data_utils import remove_all_samples
-CHUNK_SIZE = num_obs
+
+CHUNK_SIZE = num_obs # number of chip variants
 HAPS = [0,1]
+
+
+
+ref_panel_full_array = remove_all_samples(ref_panel_full_array_full_packed)
+
+#     ref_panel_full_array = remove_all_samples(ref_panel_full_array_full)
+#     ref_panel_full_array = zarr.load("./new_data/SI_data/chr1/reference_panel.30x.hg38_chr1_noinfo_full_nppacked.zip")
+
+#     ref_panel_chip_array = ref_panel_full_array[original_indicies,:]
+ref_panel_chip_array: np.ndarray = zarr.load(f"{SI_data_folder}/reference_panel.30x.hg38_{chrom}_noinfo_chip_variants_292samples_removed.zip") # type: ignore # enforce the type
+
+
+with forced_open(f"./hmm_args_dump/original_indicies.pkl", "wb") as f:
+    pickle.dump(original_indicies, f)
+with forced_open(f"./hmm_args_dump/ref_panel_full_array.pkl", "wb") as f:
+    pickle.dump(ref_panel_full_array, f)
+with forced_open(f"./hmm_args_dump/num_obs.pkl", "wb") as f:
+    pickle.dump(num_obs, f)
+with forced_open(f"./hmm_args_dump/distances_cm.pkl", "wb") as f:
+    pickle.dump(distances_cm, f)
+with forced_open(f"./hmm_args_dump/chr_length.pkl", "wb") as f:
+    pickle.dump(chr_length, f)
+
 
 for sample in samples[12:]:
     print(sample)
+
+
+    """
+    loading sample (the full true sample and the unimputed one)
+    """
     full_res_ = {}
     sample_index = get_sample_index(sample, samples_txt_path=f"./new_data/SI_data/samples.txt")
     target_full_array = np.zeros((chr_length,2))
     target_full_array[:,0] = np.unpackbits(ref_panel_full_array_full_packed[:,sample_index[0]])[:chr_length]
     target_full_array[:,1] = np.unpackbits(ref_panel_full_array_full_packed[:,sample_index[1]])[:chr_length]
 
-    ref_panel_full_array = remove_all_samples(ref_panel_full_array_full_packed)
-#     ref_panel_full_array = remove_all_samples(ref_panel_full_array_full)
-#     ref_panel_full_array = zarr.load("./new_data/SI_data/chr1/reference_panel.30x.hg38_chr1_noinfo_full_nppacked.zip")
-
-
-
-
-#     ref_panel_chip_array = ref_panel_full_array[original_indicies,:]
-    ref_panel_chip_array = zarr.load(f"{SI_data_folder}/reference_panel.30x.hg38_{chrom}_noinfo_chip_variants_292samples_removed.zip")
-    
     target_chip_array = target_full_array[original_indicies,:]
     combined_ref_panel_chip = np.concatenate([ref_panel_chip_array,target_chip_array],axis=1)
     print("Concatenated shape: ", combined_ref_panel_chip.shape)
 
+
+
     for hap in HAPS:
 
+        """
+        Call BiPBWT - the main data structure for imputation
+        """
         bidi_pbwt = BidiBurrowsWheelerLibrary(combined_ref_panel_chip.T.astype(np.int8), ref_panel_chip_array.shape[1]+hap)
         ppa_matrix = bidi_pbwt.getForward_Ppa()
         div_matrix = bidi_pbwt.getForward_Div()
@@ -177,7 +213,7 @@ for sample in samples[12:]:
 
         BI = np.zeros((num_hid,num_chip_vars))
         BJ = np.zeros((num_hid,num_chip_vars))
-        
+
         print("Build BI BJ: main imputation DS")
         for chip_var in trange(0,num_chip_vars):
             forward_pbwt_matches_=forward_pbwt_matches[:,chip_var]
@@ -187,7 +223,11 @@ for sample in samples[12:]:
             BI[:,chip_var] = forward_pbwt_matches_[forward_pbwt_index.argsort()][:num_hid]
             BJ[:,chip_var] = backward_pbwt_matches_[backward_pbwt_index.argsort()][:num_hid]
 
-            
+
+        """
+        when a chip variant doesn't have any match in the reference panel,
+        treat is as matching all the samples
+        """
         print("temporary fix: for chip_variants with no matches, all are taken as matches")
         for chip_var in trange(0,num_chip_vars):
             
@@ -196,11 +236,18 @@ for sample in samples[12:]:
                 BI[:,chip_var] = 1
                 BJ[:,chip_var] = 1
 
+
+
+        """
+        Initial composite panel.
+        Masks the matches array: take only `fl` haplotypes for each variant.
+        E.g. if `fl == matches.shape[0]`, then this is identity function.
+        """
         matches = BI + BJ -1
         fl = 13
         
         print("Creating initial composite panel")
-        composite_ = np.zeros(matches.shape)
+        composite_ = np.zeros(matches.shape) # mask (only 0s and 1s)
         best_matches = {}
         for chip_index in trange(0, matches.shape[1]):
             best_matches[chip_index] = list(np.argsort(matches[:,chip_index])[::-1][:fl])
@@ -208,22 +255,14 @@ for sample in samples[12:]:
                 composite_[hap_index ,chip_index:int(chip_index + BJ[hap_index, chip_index])] = 1
                 composite_[hap_index ,int(chip_index - BI[hap_index, chip_index] + 1):chip_index+1] = 1
 
-#         WOW = ((BI == 1).astype(int) * (BJ > 10).astype(int) * BJ)
-#         for i in trange(0, matches.shape[1]):
-#             x = np.unique(WOW[:,i], return_counts=True)
-#             if len(x[0]) < 25:
-#                 continue
-
-#             perc = np.percentile(x[1][1:],99)
-#             index_ = np.where(x[1][1:] > perc)[0]
-#             repeats = x[0][1:][index_]
-#             hapsi = []
-#             [hapsi.extend(np.where(BJ[:,i] == x)[0]) for x in range(0,len(repeats))]
-#             for hapso in hapsi:
-#                 composite_[hapso,i: i + int(BJ[hapso, i])] = 0
-
         comp_matches_hybrid = (composite_ * matches).astype(int)
+
+
         
+        """
+        1. Calculate what is the number of matches for each chip variant in the new composite ref panel
+        2. normalize the results.
+        """
         print("Calculating Haploid frequencies")
         hap_freq = {}
         for i in trange(0,matches.shape[1]):
@@ -247,20 +286,39 @@ for sample in samples[12:]:
             tmax = 1
             haps_freqs_array_norm = (((haps_freqs_array - rmin)/(rmax - rmin)) * (tmax - tmin) + tmin)
             haps_freqs_array_norm_dict[chunk_index] = haps_freqs_array_norm.copy()
-            
-        # STD_THRESH
+
+
+
+
+        """
+        1. calculates coefficients:
+         - std_thresh
+            An affine transformed (squeezed, stretched, moved) power function,
+                which calculates a coefficient for each variant,
+                    from the average matches with the new composite ref panel at this variant
+         - lengthos
+         - standard deviation
+
+        2. and uses them to calculate a final number of haploids taken for each chip index,
+            using a threshold based on
+                the number of total matches between the input sample and a reference haploid
+
+        3. Filters the composite ref panel, taking only that number of haploids
+            for each chip variant
+        """
+        # 1. STD_THRESH
         averages = []
         hybrid = (composite_ * matches).astype(int)
         for i in range(0,matches.shape[1]):
             averages.append(np.average(hybrid[np.flatnonzero(hybrid[:,i]),i]))
-    
+
         std_thresh = list(get_std(np.array(averages)))
 
-
-        std_ = []
-        lengthos = []
-        final_thresh = []
-        nc_thresh = []
+        # 2. 
+        lengthos = [] # a list of np vectors 
+        std_ = [] # a list of stds for ^^^ numpy vectors
+        final_thresh = [] # a threshold that is used to calculate nc_thresh 
+        nc_thresh = [] # how many haploids should go into each chip variant
         print("Calculating haploid count thresholds for each chip variant")
         for i in trange(0,matches.shape[1]):
             chunk_index = int(i//CHUNK_SIZE)
@@ -272,7 +330,9 @@ for sample in samples[12:]:
             nc_thresh.append(
                 len(np.flatnonzero(lengthos[i] >= final_thresh[i]))
             )
+        assert len(lengthos) == len(std_) == len(final_thresh) == len(nc_thresh) == num_obs
 
+        # 3. filter the composite ref panel with the threshold above (nc_thresh)
         composite_ = np.zeros(matches.shape)
         best_matches = {}
         for chip_index in trange(0, matches.shape[1]):
@@ -282,9 +342,13 @@ for sample in samples[12:]:
             for hap_index in best_matches[chip_index]:
                 composite_[hap_index ,chip_index:int(chip_index + BJ[hap_index, chip_index])] = 1
                 composite_[hap_index ,int(chip_index - BI[hap_index, chip_index] + 1):chip_index+1] = 1
-    
-        
-        ordered_matches_test__ = {}
+
+
+        """
+        Forms a list of ids of haploids taken for each chip variant
+            in a dict
+        """
+        ordered_matches_test__ = {} # a list of ids of haploids taken for each chip variant
         comp_to_plot = np.zeros(composite_.shape)
         for i in trange(BJ.shape[1]):
             xooi = matches[:,i]
@@ -297,6 +361,7 @@ for sample in samples[12:]:
             if len(ordered_matches_test__[i]) == 0:
                 ordered_matches_test__[i] = list(np.where(matches[:,i] != 0)[0])
         
+        # useless block: calculates a dictionary which value doesn't affect calculations
         length_matches = {}
         length_matches_normalized = {}
         for i in range(0,matches.shape[1]):
@@ -310,21 +375,35 @@ for sample in samples[12:]:
             tmax = 0
             length_matches_normalized[i] = list(np.round((((np.array(length_matches[i]) - rmin)/(rmax - rmin)) * (tmax - tmin) + tmin),5))
         
+
+
         print("Start Imputing")
+        with forced_open(f"./hmm_args_dump/{sample}/{hap}/ordered_matches_test__.pkl", "wb") as f:
+            pickle.dump(ordered_matches_test__, f)
+        with forced_open(f"./hmm_args_dump/{sample}/{hap}/length_matches_normalized.pkl", "wb") as f:
+            pickle.dump(length_matches_normalized, f)
+        with forced_open(f"./hmm_args_dump/{sample}/{hap}/num_hid.pkl", "wb") as f:
+            pickle.dump(matches.shape[0], f)
+        hmm_running_time_start = time.time()
+
+
+
         resultoo_fb = run_hmm(
-            original_indicies,
-            ref_panel_full_array,
-            num_obs,
-            ordered_matches_test__,
-            distances_cm,
-            BI, BJ,
+            original_indicies, # common
+            ref_panel_full_array, # common
+            num_obs, # common 
+            ordered_matches_test__, # haploid-specific
+            distances_cm, # common
+            BI, BJ, # unused
             # THIS VARIABLE IS USELESS, YOU CAN REFACTOR AND REMOVE
-            length_matches_normalized,
-            chr_length,
-            num_hid=matches.shape[0],
+            length_matches_normalized, # haploid-specific
+            chr_length, # common
+            num_hid=matches.shape[0], # haploid-specific
             
 #             imputed_chr_length=
         )
+        if resultoo_fb is None:
+            raise RuntimeError(f"run_hmm returned `None` instead of a numpy array")
 #         resultoo_fb = run_hmm_variable_N(
 #             original_indicies,
 #             ref_panel_full_array,
@@ -334,10 +413,13 @@ for sample in samples[12:]:
 #             nc_thresh,
 #             length_matches_normalized,
 #         )
-        START = original_indicies[0]
-        END = original_indicies[-1]
+        hmm_running_times.append(time.time() - hmm_running_time_start)
         print("DONE")
 
+
+        """
+        checking the accuracy of the imputation after the forward-backward algorithm
+        """
         new_x = target_full_array[:,hap]
         y = resultoo_fb
         print(int(y.shape - np.sum(new_x == y)))
@@ -356,9 +438,15 @@ for sample in samples[12:]:
             y_2 = target_full_array[:,1]
             final_y = y_1 + y_2
             print("Full results")
-            print(int(final_arr.shape - np.sum(final_y == final_arr)))
+            print(int(final_arr.shape - np.sum(final_y == final_arr))) # count number of individual mismatching unphased genotypes
 
-print("--- %s seconds ---" % (time.time() - start_time))
+
+
+print("--- hmm running times ---")
+print(hmm_running_times)
+print(f"avg: {sum(hmm_running_times)/len(hmm_running_times)}")
+print(f"std: {np.std(np.array(hmm_running_times))}")
+print("--- Total time: %s seconds ---" % (time.time() - start_time))
 
 # In[ ]:
 
@@ -371,7 +459,7 @@ def get_beagle_res(file_name):
         beagle_array.applymap(lambda x: str(x).split(":")[0].replace("/","|").split("|")[-1]),
     ],axis=1)
     concat_beagle.drop([2],axis=1,inplace=True)
-    concat_beagle.columns = [0,1]
+    concat_beagle.columns = [0,1] # type: ignore
     return concat_beagle
 
 
@@ -399,30 +487,30 @@ print(int(final_arr_beagle.shape - np.sum(final_y == final_arr_beagle)))
 # In[ ]:
 
 
-# THIS PLOT DOESN"T WORK YET NOW
+# # THIS PLOT DOESN"T WORK YET NOW
 
-# PLOT results
+# # PLOT results
 
-# del plot_results
-import utils_no_mod
-# importlib.reload(utils_no_mod)
-from utils_no_mod import plot_results
-folder_dict = {
-    "beagle":"/home/ubuntu/selphi/data/validation_data/beagle_validated/",
-#     "selphi HMM":"./",
-    "selphi HMM":"/home/ubuntu/selphi/method_first_draft/",
-}
+# # del plot_results
+# import utils_no_mod
+# # importlib.reload(utils_no_mod)
+# from utils_no_mod import plot_results
+# folder_dict = {
+#     "beagle":"/home/ubuntu/selphi/data/validation_data/beagle_validated/",
+# #     "selphi HMM":"./",
+#     "selphi HMM":"/home/ubuntu/selphi/method_first_draft/",
+# }
 
-# OVERWRITE SAMPLES
-# samples = [
-#     "HG02470",
-# ]
+# # OVERWRITE SAMPLES
+# # samples = [
+# #     "HG02470",
+# # ]
 
-plot_results(
-    samples,
-    folder_dict,
-    ref_panel_full_array_full,
-    target_full_array,
-    original_indicies,
-)
+# plot_results(
+#     samples,
+#     folder_dict,
+#     ref_panel_full_array_full,
+#     target_full_array,
+#     original_indicies,
+# )
 
