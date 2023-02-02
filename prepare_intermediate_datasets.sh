ae866c1d24aebce8d59e9ee9c53a05e94595319e
Mykyta Matushyn <nikita@selfdecode.com>
2023-02-02 11:26:03 +0000

    added checks for bad arguments to `prepare_intermediate_datasets.sh`

--- prepare_intermediate_datasets.sh
+++ prepare_intermediate_datasets.sh
@@ -5,6 +5,23 @@ chipsites_input="$1"
 fullseq_refpan="$2"
 
 
+if [[ -z "$fullseq_refpan" ]]; then
+    echo -e "usage:"
+    echo -e " bash \"$0\" <input chip-sites dataset.vcf.gz> <reference panel dataset.vcf.gz>"
+    exit 10
+fi
+if [[ ! -f "$chipsites_input" ]]; then
+    echo -e "ERROR: input dataset file does not exist at:"
+    echo -e " • \"$chipsites_input\""
+    exit 11
+fi
+if [[ ! -f "$fullseq_refpan" ]]; then
+    echo -e "ERROR: reference panel dataset file does not exist at:"
+    echo -e " • \"$fullseq_refpan\""
+    exit 12
+fi
+
+
 echo -e "1. slicing only chip sites (input samples sites) from the reference panel ..."
 time { bash "${thisdir}"/cr8-chip-sites-vcf.sh "$chipsites_input" "$fullseq_refpan" ; }
 
