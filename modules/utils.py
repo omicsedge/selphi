3cc0378b85a12f06fd5dfe99e85fd06103f8c590
sandra <sandra@selfdecode.com>
2023-06-15 14:21:26 +0000

    remove unused code

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,3 @@
-import subprocess
-from io import StringIO
-import pandas as pd
 import numpy as np
 
 
@@ -19,56 +16,3 @@ def get_std(avg_length: np.ndarray, a: int = 25) -> np.ndarray:
     tmin_ = 0.2
     tmax_ = 3
     return ((std_not_normed - rmin_) / (rmax_ - rmin_)) * (tmax_ - tmin_) + tmin_
-
-
-def get_variants_bcf(input_bcfgz_path):
-    """
-    return a list of variants in the provided bcf
-    """
-    cmd_str = f'bcftools view {input_bcfgz_path} | grep -v "##" '
-    result = subprocess.run(
-        cmd_str,
-        shell=True,
-        stdout=subprocess.PIPE,
-    )
-    s = str(result.stdout, "utf-8")
-    data = StringIO(s)
-    df = pd.read_table(
-        data,
-        usecols=[
-            "ID",
-        ],
-    )
-    return list(df["ID"])
-
-
-def get_samples_bcf(input_bcfgz_path):
-    """
-    return a list of samples in the provided bcf
-    """
-    cmd_str = f"bcftools query -l {input_bcfgz_path}"
-    result = subprocess.run(
-        cmd_str,
-        shell=True,
-        stdout=subprocess.PIPE,
-    )
-    s = str(result.stdout, "utf-8")
-    data = StringIO(s)
-    df = pd.read_table(data, header=None)
-    return list(df[0])
-
-
-def get_samples_xsi(xsi_file):
-    """
-    return a list of samples in the provided xsi files
-    """
-    cmd_str = f"xsqueezeit -x -r 0 -f {xsi_file} | bcftools query -l"
-    result = subprocess.run(
-        cmd_str,
-        shell=True,
-        stdout=subprocess.PIPE,
-    )
-    s = str(result.stdout, "utf-8")
-    data = StringIO(s)
-    df = pd.read_table(data, header=None)
-    return list(df[0])
