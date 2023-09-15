4252573de06cddc369574a4b128e16e375a27ddd
sandra-selfdecode <sandra@selfdecode.com>
2023-09-14 17:05:37 -0700

    update runs

--- run_selphi.sh
+++ run_selphi.sh
@@ -5,18 +5,18 @@ BATCH=$2
 L=$3
 
 BUCKET=rd-imputation-project
-PROJECT=adriano/selphi/ukb_gwas
-REF=chr${CHROM}_ukb_100k
-TARGET=chr${CHROM}_target_$BATCH.vcf.gz
+PROJECT=shared/reference_panel
+REF=chr${CHROM}_ukb_150k
+TARGET=chr${CHROM}_targets_batch_$BATCH.vcf.gz
 OUTPUT=selphi_chr${CHROM}_batch_${BATCH}_L$L
 
 # download input
 aws s3 cp s3://$BUCKET/shared/resource_files/maps/plink.chr$CHROM.GRCh38.map . --quiet
-aws s3 cp s3://$BUCKET/$PROJECT/reference_100k/srp/$REF.pbwt . --quiet
-aws s3 cp s3://$BUCKET/$PROJECT/reference_100k/srp/$REF.samples . --quiet
-aws s3 cp s3://$BUCKET/$PROJECT/reference_100k/srp/$REF.sites . --quiet
-aws s3 cp s3://$BUCKET/$PROJECT/reference_100k/srp/$REF.srp . --quiet
-aws s3 cp s3://$BUCKET/$PROJECT/target_50k/chr$CHROM/$TARGET . --quiet
+aws s3 cp s3://$BUCKET/$PROJECT/ukbb_150k_wgs/selphi/$REF.pbwt . --quiet
+aws s3 cp s3://$BUCKET/$PROJECT/ukbb_150k_wgs/selphi/$REF.samples . --quiet
+aws s3 cp s3://$BUCKET/$PROJECT/ukbb_150k_wgs/selphi/$REF.sites . --quiet
+aws s3 cp s3://$BUCKET/$PROJECT/ukbb_150k_wgs/selphi/$REF.srp . --quiet
+aws s3 cp s3://$BUCKET/$PROJECT/genotypeCalls_ukb_500k/targets/chr$CHROM/$TARGET . --quiet
 
 # record system usage every 5 seconds while running
 vmstat -t -S MB > $OUTPUT.metrics # include headers in first write
@@ -33,7 +33,7 @@ python3 /tool/selphi.py \
     2>&1 | tee -a $OUTPUT.log
 
 # upload result
-DESTINATION=s3://$BUCKET/$PROJECT/imputed/imputed_selphi/vcf/chr$CHROM/
+DESTINATION=s3://$BUCKET/$PROJECT/genotypeCalls_ukb_500k/imputed_selphi/vcf/chr$CHROM/
 aws s3 cp $OUTPUT.vcf.gz $DESTINATION --quiet
 aws s3 cp $OUTPUT.vcf.gz.tbi $DESTINATION --quiet
 aws s3 cp $OUTPUT.log $DESTINATION --quiet
